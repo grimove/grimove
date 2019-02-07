2ae2cb74b240e0ae5466045790d451e95345738c
grimove <47428475+grimove@users.noreply.github.com>
2019-02-07 18:47:11 +0100

    First commit

---
+++ Vaja.java
@@ -0,0 +1,6 @@
+public class Vaja{
+  public static void main (String[]args) {
+    System.out.println ("Lep pozdrav!");
+    // sistemski izhod (cmd) naj izpise ta text, pomeni systemoutprint in mora biti vedno ; ker je to ukazna vrstica
+  }
+}
